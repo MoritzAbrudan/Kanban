9f324d4a8c4f54645d3d205c1c839c0d4a634095
MoritzAbrudan <moritzabrudan@hotmail.de>
2021-09-22 11:48:24 +0200

    addTask.js V1

--- addTask.js
+++ addTask.js
@@ -1,9 +1,23 @@
-let allTasks = [];
+let allTasks = [{
+    "title": "Kanban Board erstellen",
+    "category": "Management",
+    "description": "Gruppenarbeit bei der Developerakademie",
+    "date": "08.10.2021",
+    "urgency": "normal",
+    "assignedTo": "alle",
+}, ];
 
 function clearAll() {
-    document.getElementById('createTask').reset();
+    document.getElementById('addTask').reset();
 }
 
 function createTask() {
-
+    allTasks.push({
+        title: document.getElementById('title').value,
+        //category: document.getElementById('category').value,
+        //description: document.getElementById('description').value,
+        //date: document.getElementById('date').value,
+        //urgency: document.getElementById('urgency').value,
+        //assignedTo: document.getElementById('assignedTo').value,
+    }, );
 }

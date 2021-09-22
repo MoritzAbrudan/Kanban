b4a1a42bd6611b44e88a7f0a7c6281258ba5115f
MoritzAbrudan <moritzabrudan@hotmail.de>
2021-09-22 14:08:00 +0200

    addTask.js V2

--- addTask.js
+++ addTask.js
@@ -7,10 +7,58 @@ let allTasks = [{
     "assignedTo": "alle",
 }, ];
 
+let category = ['Management', 'Marketing', 'Retail', 'Accounting'];
+let urgency = ['Very High', 'High', 'Normal', 'Got Time'];
+
+/**
+ * 
+ * Collection of functions wich are initiated onload
+ */
+function init() {
+    defineUrgency();
+    defineUrgency();
+}
+
+/**
+ * 
+ * Function for dropdown menu Category
+ */
+function defineCategory() {
+    document.getElementById('category').innerHTML = '';
+    for (let i = 0; i < category.length; i++) {
+        const cat = category[i];
+        document.getElementById('category').innerHTML += `
+        <option value="${cat}"></option>
+        `;
+    };
+}
+
+/**
+ * 
+ * Function for dropdown menu Urgency
+ */
+function defineUrgency() {
+    document.getElementById('urgency').innerHTML = '';
+    for (let i = 0; i < urgency.length; i++) {
+        const urge = urgency[i];
+        document.getElementById('urgency').innerHTML += `
+        <option value="${urge}"></option>
+        `;
+    };
+}
+
+/**
+ * 
+ * Clears all input field of the form
+ */
 function clearAll() {
     document.getElementById('addTask').reset();
 }
 
+/**
+ * 
+ * saves new task in array
+ */
 function createTask() {
     allTasks.push({
         title: document.getElementById('title').value,
